20b406704de2b4a6fd5544eef89bfe5eb471e8db
esky2674 <ester.kylmanen.2674@student.uu.se>
2020-09-25 13:09:55 +0200

    new settings

--- registrationSystem/registrationSystem/wsgi.py
+++ registrationSystem/registrationSystem/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'registrationSystem.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'registrationSystem.settings.dev')
 
 application = get_wsgi_application()
