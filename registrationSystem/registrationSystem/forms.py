8092de9e3ce373820188a1c9ab4bd82d8307765e
esky2674 <ester.kylmanen.2674@student.uu.se>
2020-10-08 19:17:26 +0200

    added personnummerfield

--- registrationSystem/registrationSystem/forms.py
+++ registrationSystem/registrationSystem/forms.py
@@ -1,8 +1,12 @@
 from django.forms import ModelForm
 from registrationSystem.models import InterestCheck
+from registrationSystem.fields import PersonNumberField
 
 
 class InterestCheckForm(ModelForm):
+
+    personnr = PersonNumberField()
+
     class Meta:
         model = InterestCheck
         fields = ['name',
