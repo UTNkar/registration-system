5fd24ba88a01bb97ebe0217d988cc1dc448b49ec
esky2674 <ester.kylmanen.2674@student.uu.se>
2020-10-08 19:17:13 +0200

    submit form

--- registrationSystem/registrationSystem/views.py
+++ registrationSystem/registrationSystem/views.py
@@ -2,12 +2,21 @@ from django.shortcuts import render, redirect
 from registrationSystem.models import InterestCheck
 from registrationSystem.forms import InterestCheckForm
 
+
 async def start(request):
-    if request.method == 'POST':
+    if request.POST:
         form = InterestCheckForm(request.POST)
+
         if form.is_valid():
-            form.save()
-            return redirect('/ticket-system/')
+            interest_check_obj = InterestCheck(
+                name=form.cleaned_data['name'],
+                email=form.cleaned_data['email'],
+                personnr=form.cleaned_data['personnr']
+            )
+
+            return render(request,
+                          "status_page.html",
+                          {'interest_user': interest_check_obj})
     else:
         form = InterestCheckForm()
     return render(request, "start_page.html", {'form': form})
