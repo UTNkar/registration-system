07db83a23be1f83ee1c9a8d8c55a8de4d7ac147f
Gustav Thaung Tolförs <gustav.thaung.tolfors@utn.se>
2020-10-20 20:16:59 +0200

    Get user or 404

--- registrationSystem/registrationSystem/views.py
+++ registrationSystem/registrationSystem/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import RiverraftingUser
 
 def create_account(request, uid):
-    user= RiverraftingUser.objects.get(id=uid)
+    user = get_object_or_404(RiverraftingUser, id=uid)
     context = {
         # 'uid':
         'name': user.name,
