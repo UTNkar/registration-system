d65d528f66f151e2a4d5d82613f85a9d145ce028
Gustav Thaung Tolförs <gustav.thaung.tolfors@utn.se>
2020-10-20 20:01:01 +0200

    Getting and displaying user information

--- registrationSystem/registrationSystem/views.py
+++ registrationSystem/registrationSystem/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import RiverraftingUser
 
-def create_account(request):
+def create_account(request, uid):
+    user= RiverraftingUser.objects.get(id=uid)
     context = {
-        'uid': '123a'
+        # 'uid':
+        'name': user.name,
+        'personnummer': user.person_nr,
+        'email': user.email
     }
     return render(request, 'registrationSystem/create_account.html', context)
