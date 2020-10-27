6ba9417e9c5a72789947cfc936458c9b67a397f6
Gustav Thaung Tolförs <gustav.thaung.tolfors@utn.se>
2020-10-27 19:40:11 +0100

    Initial form values and disabled fields

--- registrationSystem/registrationSystem/forms.py
+++ registrationSystem/registrationSystem/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, TextInput, EmailInput
 from registrationSystem.models import InterestCheck, RiverraftingUser
 
 
@@ -16,4 +16,12 @@ class CreateAccountForm(ModelForm):
         fields = [  'name',
                     'person_nr',
                     'email',
-                    'password']
+                    'password' ]
+
+        labels = {  'name': 'Namn',
+                    'person_nr': 'Personnummer',
+                    'email': 'E-postadress' }
+
+        widgets = { 'name': TextInput(attrs={'disabled': True}),
+                    'person_nr': TextInput(attrs={'disabled': True}),
+                    'email': EmailInput(attrs={'disabled': True}) }
--- registrationSystem/registrationSystem/views.py
+++ registrationSystem/registrationSystem/views.py
@@ -4,13 +4,16 @@ from .models import RiverraftingUser
 from .forms import CreateAccountForm
 
 def create_account(request, uid):
-    form = CreateAccountForm()
     user = get_object_or_404(RiverraftingUser, id=uid)
+    form = CreateAccountForm(initial={
+                                    'name': user.name,
+                                    'person_nr': user.person_nr,
+                                    'email': user.email })
     context = {
         # 'uid':
         'form': form,
-        'name': user.name,
-        'person_nr': user.person_nr,
-        'email': user.email
+        # 'name': user.name,
+        # 'person_nr': user.person_nr,
+        # 'email': user.email
     }
     return render(request, 'registrationSystem/create_account.html', context)
