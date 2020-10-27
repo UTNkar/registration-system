310c5f1f9fef5e7696cfb3a59a7c1392163055a5
Gustav Thaung Tolförs <gustav.thaung.tolfors@utn.se>
2020-10-27 18:57:50 +0100

    Replaced with ModelForm

--- registrationSystem/registrationSystem/forms.py
+++ registrationSystem/registrationSystem/forms.py
@@ -1,5 +1,5 @@
 from django.forms import ModelForm
-from registrationSystem.models import InterestCheck
+from registrationSystem.models import InterestCheck, RiverraftingUser
 
 
 class InterestCheckForm(ModelForm):
@@ -7,4 +7,13 @@ class InterestCheckForm(ModelForm):
         model = InterestCheck
         fields = ['name',
                   'email',
-                  'personnr']
+                  'person_nr']
+
+
+class CreateAccountForm(ModelForm):
+    class Meta:
+        model = RiverraftingUser
+        fields = [  'name',
+                    'person_nr',
+                    'email',
+                    'password']
--- registrationSystem/registrationSystem/views.py
+++ registrationSystem/registrationSystem/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import RiverraftingUser
+from .forms import CreateAccountForm
 
 def create_account(request, uid):
+    form = CreateAccountForm()
     user = get_object_or_404(RiverraftingUser, id=uid)
     context = {
         # 'uid':
+        'form': form,
         'name': user.name,
         'person_nr': user.person_nr,
         'email': user.email
