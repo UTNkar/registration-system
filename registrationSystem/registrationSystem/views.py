9aeadcd6b3d9d7147c4c00d8a197156f3f6f9680
Gustav Thaung Tolförs <gustav.thaung.tolfors@utn.se>
2020-11-01 12:02:56 +0100

    Corrected initializing of form

--- registrationSystem/registrationSystem/views.py
+++ registrationSystem/registrationSystem/views.py
@@ -5,10 +5,14 @@ from .forms import CreateAccountForm
 
 def create_account(request, uid):
     user = get_object_or_404(InterestCheck, id=uid)
-    form = CreateAccountForm(request.POST or None)
-    # initial={  'name': user.name,
-    #                                                     'person_nr': user.person_nr,
-    #                                                     'email': user.email }   )
+
+    if request.method == "POST":
+        form = CreateAccountForm(request.POST)
+    else:
+        form = CreateAccountForm(initial={ 'name': user.name, 'person_nr': user.person_nr, 'email': user.email })
+        # Doing it this way avoids triggering validation errors since the password fields are not
+        # initially filled.
+        # Initializing form = CreateAccountForm(request.POST or initial={...}) triggers the errors.
 
     if form.is_valid():
         # Linus jobbar på en lösning för att automatiskt hasha alla lösenord i databasen.
