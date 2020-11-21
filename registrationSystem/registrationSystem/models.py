f1c01945ce57ab750dd9549647362f356e2a449f
Linus Shoravi <linusshoravi@gmail.com>
2020-11-21 17:02:32 +0100

    Document ImportantDate model

--- registrationSystem/registrationSystem/models.py
+++ registrationSystem/registrationSystem/models.py
@@ -182,6 +182,15 @@ class EmailConfirmations(models.Model):
 
 
 class ImportantDate(models.Model):
+    """
+    Important event dates.
+
+    An important date is dates on which events or deadlines happen.
+
+    'On the 14th of february there will be a raffle pub.'
+    'On the 1st of March payment closes.'
+    And so on.
+    """
     date = models.DateField(verbose_name="Date")
     desc = models.CharField(max_length=254, verbose_name="Description")
 
