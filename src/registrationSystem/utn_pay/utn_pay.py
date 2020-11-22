5a47706153dbb309928ce7fdfdf8cfe4e623ba00
Daniel Fehrm <daniel@fehrm.se>
2020-11-22 19:25:12 +0100

    added documentation to get payment link

--- src/registrationSystem/utn_pay/utn_pay.py
+++ src/registrationSystem/utn_pay/utn_pay.py
@@ -5,12 +5,36 @@ from .payment_models.forska_payment import ForskaPayment
 
 
 def get_order_rows_model():
+    """
+    Returns the payment model for the event set in settings
+    """
     if settings.EVENT == 'RIVERRAFTING':
         return ForskaPayment()
 
 
 def get_payment_link(team_leader):
-    rows, total_cost = get_order_rows_model().get_items_and_cost(team_leader)
+    """
+    Creates a payment on pay.utn.se and returns the link to where
+    a user will make the payment
+
+    Params:
+
+    team_leader: A user that is also the team leader.
+
+    Returns:
+
+    A link to pay.utn.se where the user should go to make their payment
+
+    Raises:
+
+    ValueError if team_leader is not a team leader or if pay
+    gives an invalid response
+    """
+    if not team_leader.is_team_leader():
+        raise ValueError("User is not a team leader")
+
+    order_rows_model = get_order_rows_model()
+    rows, total_cost = order_rows_model.get_order_rows_and_cost(team_leader)
 
     # Pay.utn.se wants first and last name while we store the first and last
     # name as a whole. Therefor we split the name to fake a last name
