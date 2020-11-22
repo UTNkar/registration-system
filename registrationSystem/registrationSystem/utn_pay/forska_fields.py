d9e4e62be4fe300afc84f2e622980b7491085714
antonfogelberg <antonfogelberg@gmail.com>
2020-11-22 16:39:35 +0100

    Added new file for items to buy

---
+++ registrationSystem/registrationSystem/utn_pay/forska_fields.py
@@ -0,0 +1,24 @@
+forska_fields = {
+    'raft_fee': {
+        'name': "Raft fee",
+        'description': "The base fee for a raft in the River Rafting",
+        'quantity': 1,
+        'unit': 'st',
+        'amount': 10000,  # Must be in ören
+    },
+    'lifevest': {
+        'name': "Life vests",
+        'description': "Make you float. floaet = good!",
+        'unit': 'st',
+    },
+    'wetsuit': {
+        'name': "Wetsuit",
+        'description': "Wetsuit to survive water when cold",
+        'unit': 'st',
+    },
+    'helmet': {
+        'name': "Helmet",
+        'description': "Protect head! Head dmg -1",
+        'unit': 'st',
+    },
+}
