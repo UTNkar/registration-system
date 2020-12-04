e125d19cb24a046f8fc8ead0572fe83ed6897afd
Daniel Fehrm <daniel@fehrm.se>
2020-12-04 14:12:34 +0100

    Personnummer can only be one format (#64)
    * moved ssn validator to new file and allow only one format
    
    * use ssn validator as default for ssn field
    
    * added tests for ssn validator
    
    * use sqlite3 for tests

--- src/registrationSystem/tests.py
+++ src/registrationSystem/tests.py
@@ -1,6 +1,35 @@
 from django.test import TestCase
+from django.core.exceptions import ValidationError
+from registrationSystem.validators import SSNValidator
 from registrationSystem.forms import RaffleEntryForm
 from registrationSystem.models import RaffleEntry
+
+
+class SSNValidatorTest(TestCase):
+    def test_long_ssn(self):
+        try:
+            SSNValidator()("19980521-1234")
+        except ValidationError:
+            self.fail("Valid SSN raised a validation error")
+
+    def test_long_ssn_with_t(self):
+        try:
+            SSNValidator()("19980521-T234")
+            SSNValidator()("19980521-t234")
+        except ValidationError:
+            self.fail("Valid SSN raised a validation error")
+
+    def test_long_ssn_without_dash(self):
+        with self.assertRaises(ValidationError):
+            SSNValidator()("199805211234")
+
+    def test_short_ssn(self):
+        with self.assertRaises(ValidationError):
+            SSNValidator()("980521-1234")
+
+    def test_short_ssn_without_dash(self):
+        with self.assertRaises(ValidationError):
+            SSNValidator()("9805211234")
 
 
 class RaffleEntryFormTest(TestCase):
